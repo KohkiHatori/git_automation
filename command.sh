909f4a279db9dcbdff2521716647f834decc4e88
kohkhatori <kouki20040124hatori@gmail.com>
2022-07-12 17:42:08 +0900

    commit command

---
+++ command.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function mkcdir(){
+	mkdir -p -- "$1" &&
+	cd -P -- "$1"
+}
+
+function create(){
+	mkcdir $1
+	git init
+	curl -H 'Authorization: token '"$PAT"'' --data '{"name":"'"$1"'"}' https://api.github.com/user/repos	
+	git remote add origin git@github.com:KohkiHatori/$1.git
+	touch README.md
+	git add .
+	git commit -m "initial commit"
+	git push -u origin master
+}
